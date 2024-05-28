b9f715766d6380037dc515877ff4ac4f552d6287
eve seni <senieve@gmail.com>
2024-05-28 17:59:00 +0200

    progress bar

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -5,11 +5,20 @@ import { Dialog, DialogTrigger, DialogContent } from "./ui/dialog";
 import { Button } from "./ui/button";
 import Dropzone from "react-dropzone";
 import { useDropzone } from "react-dropzone";
+import { Cloud, File, Loader2 } from "lucide-react";
+import { Progress } from "./ui/progress";
 
 const UploadDropZone = () => {
+  const [isUploading, setIsUploading] = useState<boolean>(true);
+
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
   return (
-    <Dropzone multiple={false}>
+    <Dropzone
+      multiple={false}
+      onDrop={(acceptedFiles) => {
+        console.log(acceptedFiles);
+      }}>
       {({ getRootProps, getInputProps, acceptedFiles }) => (
         <div
           {...getRootProps()}
@@ -17,9 +26,40 @@ const UploadDropZone = () => {
           <div className="flex items-center  justify-center h-full w-full ">
             <label className="flex flex-col items-center w-full h-full justify-center rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
               <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                {" "}
-                exemple
+                <Cloud className="mb-2 h-6 w-6 text-zinc-500" />
+                <p className="text-zinc-700 mb-2 text-sm">
+                  Click to upload or drag and drop
+                </p>
+                <p className="text-zinc-500 text-xs"> PDF (up to 4mb)</p>
               </div>
+
+              {/* little box showing uploaded file */}
+              {acceptedFiles && acceptedFiles[0] ? (
+                <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
+                  <div className="px-3 py-2 h-full grid place-items-center">
+                    <File className="h-4 w-4 text-blue-500" />
+                  </div>
+                  <div className="px-3 py-2 h-full text-sm truncate">
+                    {acceptedFiles[0].name}
+                  </div>
+                </div>
+              ) : null}
+              {/* progress baar during upload */}
+              {isUploading ? (
+                <div className="w-full mt-4 max-w-xs mx-auto">
+                  <Progress
+                    color={uploadProgress === 100 ? "bg-green-500" : ""}
+                    value={uploadProgress}
+                    className="h-1 w-full bg-zinc-200"
+                  />
+                  {uploadProgress === 100 ? (
+                    <div className="flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2">
+                      <Loader2 className="h-3 w-3 animate-spin" />
+                      Redirecting...
+                    </div>
+                  ) : null}
+                </div>
+              ) : null}
             </label>
           </div>
         </div>
