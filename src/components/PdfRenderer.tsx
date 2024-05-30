15b57c5449fec2d8c0daeb785f5fa19106c30f57
eve seni <senieve@gmail.com>
2024-05-30 21:05:04 +0200

    stylign width fixed

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,9 +1,13 @@
 "use client";
+import { Loader2 } from "lucide-react";
 import React from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
+import { useToast } from "./ui/use-toast";
+import { useResizeDetector } from "react-resize-detector";
 
+//for reatc-pdf to work
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 interface PdfRendererProps {
@@ -11,6 +15,8 @@ interface PdfRendererProps {
 }
 
 const PdfRenderer = ({ url }: PdfRendererProps) => {
+  const { toast } = useToast();
+  const { width, ref } = useResizeDetector();
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
@@ -18,9 +24,24 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
       </div>
 
       <div className="flex-1 w-full max-h-screen">
-        <div>
-          <Document className="max-h-full" file={url}>
-            <Page pageNumber={1} />
+        {/* pdf fit to page */}
+        <div ref={ref}>
+          <Document
+            loading={
+              <div className="flex justify-center">
+                <Loader2 className="animate-spin my-24 h-6 w-6" />
+              </div>
+            }
+            onLoadError={() => {
+              toast({
+                title: "error loading PDF",
+                description: "try again later",
+                variant: "destructive",
+              });
+            }}
+            className="max-h-full"
+            file={url}>
+            <Page width={width ? width : 1} pageNumber={1} />
           </Document>
         </div>
       </div>
