f77204429b3f9c1f82ea7f7d8cfc99837e3ee7e4
eve seni <senieve@gmail.com>
2024-06-03 10:59:02 +0200

    suspense fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+  experimental: {
     turbo: {
       resolveAlias: {
         canvas: "./empty-module.ts",
--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const authCallback = () => {
   const router = useRouter;
+
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
 };
