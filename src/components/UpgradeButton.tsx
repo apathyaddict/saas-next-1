ad454d625b1d7f99a1a479378fe51c84b4273045
eve seni <senieve@gmail.com>
2024-06-03 09:53:49 +0200

    upgrade button func

--- src/components/UpgradeButton.tsx
+++ src/components/UpgradeButton.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
+
+import { CirclePlus } from "lucide-react";
+import { Button } from "./ui/button";
+import { useToast } from "./ui/use-toast";
 
 const UpgradeButton = () => {
-  return <div>UpgradeButton</div>;
+  const { toast } = useToast();
+
+  return (
+    <Button
+      onClick={() => {
+        toast({
+          title: "Upgrade or payment not enabled. ",
+          description: "This is for looks only.",
+        });
+      }}
+      className="w-full">
+      Upgrade now <CirclePlus className="h-5 w-5 ml-1.5" />
+    </Button>
+  );
 };
 
 export default UpgradeButton;
